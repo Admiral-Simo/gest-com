b6d55e85d4af5028ace3f418f1c31ee01bdc6a7d
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-10 14:18:21 +0100

    feat: make a selling table

--- src/app/selling/selling.component.ts
+++ src/app/selling/selling.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-selling',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './selling.component.html',
-  styleUrl: './selling.component.css'
 })
 export class SellingComponent {
+  data: SellingInterface[];
 
+  job: string = '';
+  name: string = '';
+  favColor: string = '';
+
+  constructor() {
+    this.data = [
+      { Job: 'Job 1', Name: 'Name 1', FavColor: 'Color 1' },
+      { Job: 'Job 2', Name: 'Name 2', FavColor: 'Color 2' },
+      { Job: 'Job 3', Name: 'Name 3', FavColor: 'Color 3' },
+    ];
+  }
+
+  addItem() {
+    let newItem: SellingInterface = {
+      Job: this.job,
+      Name: this.name,
+      FavColor: this.favColor,
+    };
+
+    this.data.push(newItem);
+
+    this.job = '';
+    this.name = '';
+    this.favColor = '';
+  }
 }
+
+type SellingInterface = {
+  Job: string;
+  Name: string;
+  FavColor: string;
+};
